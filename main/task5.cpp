d9001a1458004570abafd5cdd9822760988c88ac
Hita Juneja <hitajuneja@gmail.com>
2023-02-14 15:16:47 +0530

    Task 5 lessgo

---
+++ main/task5.cpp
@@ -0,0 +1,10 @@
+//File: task5.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hey! This is task 5's cpp file :) \n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run the pipeline\n";
+  return 0;
+}
